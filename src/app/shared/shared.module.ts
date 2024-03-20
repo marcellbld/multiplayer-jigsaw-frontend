ea2fc21f7a96989e7ff84a1e95ecbe81face5e2d
Bőti Marcell <de9d6m@inf.elte.hu>
2024-03-20 10:51:28 +0100

    ui fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainPanelComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -19,7 +21,9 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatInputModule,
     MatCardModule,
     MatSlideToggleModule,
-    MatGridListModule
+    MatGridListModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     MatToolbarModule,
@@ -29,7 +33,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatInputModule,
     MatCardModule,
     MatSlideToggleModule,
-    MatGridListModule
-  ]
+    MatGridListModule,
+    MainPanelComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.16:8080',
-  wsApiUrl: 'http://192.168.0.16:8080/ws'
+  apiUrl: 'http://192.168.1.71:8080',
+  wsApiUrl: 'http://192.168.1.71:8080/ws'
 };
