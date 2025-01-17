cbc8b6d49398dd08aeb4e5e8450ee5d50591809b
Marcell <50955131+marcellbld@users.noreply.github.com>
2025-01-17 19:41:32 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://000339.xyz:8080',
-  wsApiUrl: 'https://000339.xyz:8080/ws'
+  apiUrl: 'https://000339.xyz',
+  wsApiUrl: 'https://000339.xyz/ws'
 };
